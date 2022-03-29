869d93b0d8118ca1aca9cd7517f83f1baf75d143
Viktoria <vizu@html24.net>
2022-03-29 10:25:48 +0200

    basic todo list with filter

---
+++ to-do_app/todo.js
@@ -0,0 +1,87 @@
+const todos = [{
+    text: 'Order cat food',
+    completed: false
+}, {
+    text: 'Clean kitchen',
+    completed: true
+}, {
+    text: 'Buy food',
+    completed: true
+}, {
+    text: 'Do work',
+    completed: false
+}, {
+    text: 'Exercise',
+    completed: true
+}]
+
+let filters = {
+    searchText: '',
+    hideCompleted: false
+}
+
+const todoList = document.querySelector('.todo-list');
+const todoSummary = document.querySelector('.todo-subtitle');
+
+const renderTodos = (todos, filters) => {
+    
+    // let filteredTodos = todos.filter(function(todo){
+    //     const searchMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase()) 
+    //     const hideCompletedMatch = !filters.hideCompleted || !todo.completed;
+    //     return searchMatch && hideCompletedMatch
+    // });
+
+    let filteredTodos = todos.filter(function(todo){
+        return todo.text.toLowerCase().includes(filters.searchText.toLowerCase());
+    })
+
+    filteredTodos = filteredTodos.filter(function(todo){
+        if(filters.hideCompleted){
+            return !todo.completed
+        } else {
+            return true
+        }
+    })
+
+
+    let incompletedTodos = filteredTodos.filter(function(todo){return !todo.completed});
+
+    todoList.innerHTML = "";
+
+    todoSummary.textContent = `You have ${incompletedTodos.length} incompleted tasks`
+
+    filteredTodos.forEach((todo) => {
+        let todoItem = document.createElement('li');
+        todoItem.classList.add('todo-item');
+        todoItem.textContent = todo.text;
+        todoList.appendChild(todoItem);
+    });
+}
+
+renderTodos(todos, filters);
+
+const checkbox = document.querySelector('.hide-completed');
+const searchInput = document.querySelector('.search-text');
+
+checkbox.addEventListener('change', (event) => {
+    filters.hideCompleted = event.target.checked // true or false
+    renderTodos(todos, filters)
+})
+
+searchInput.addEventListener('input', (event) => {
+    filters.searchText = event.target.value
+    renderTodos(todos, filters)
+})
+
+const addInput = document.querySelector('.add-input');
+const addBtn = document.querySelector('.add-btn');
+
+addBtn.addEventListener('click', function(event) {
+    event.preventDefault()
+    todos.push({
+        text: addInput.value,
+        completed: false
+    })
+    renderTodos(todos, filters)
+    addInput.value = ''
+})
